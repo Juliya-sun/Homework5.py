92ad2ea4b443ada652823fb524ab42f04dbdce60
Juliya-sun <lyubimova.yulya.29@yandex.ru>
2024-07-25 13:43:03 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+immutable_var = (153, 'sun', 2.5)
+print (immutable_var)
+# immutable_var [0] = [2000]
+mutable_list = ([1, 5, 3], 'sun', 2.5)
+print (mutable_list)
+mutable_list [0] [0] = [20]
+print (mutable_list)
